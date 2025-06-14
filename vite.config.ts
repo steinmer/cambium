1d3edf9f1eacda302c8bb7bf376c4d0c0e293905
Raymond Steinmetz <rsteinmetz@mindresearch.org>
2025-06-14 17:45:19 -0400

    adding dist again

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { assetpackPlugin } from "./scripts/assetpack-vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/cambium/",
+  base: "/dist/",
   plugins: [assetpackPlugin()],
   server: {
     port: 8080,
